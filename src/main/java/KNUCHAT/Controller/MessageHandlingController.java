a4b4bd76eb407de2736b28b23aaf5c893603dae7
SangHee Lee <lsh277604@gmail.com>
2023-11-19 17:53:46 +0900

    Fix: connect KafkaService

--- src/main/java/KNUCHAT/Controller/MessageHandlingController.java
+++ src/main/java/KNUCHAT/Controller/MessageHandlingController.java
@@ -1,6 +1,8 @@
 package KNUCHAT.Controller;
 
 import KNUCHAT.Domain.ChatMessage;
+import KNUCHAT.Service.KafkaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class MessageHandlingController {
+    private final KafkaService kafkaService;
+
+
     @MessageMapping("/{id}") // pub
     @SendTo("/sub/room/{id}") // Sub
     public ChatMessage testMessage(@DestinationVariable("id") Long id,
                                    ChatMessage chatMessage) throws Exception{
-        log.info(chatMessage.getName());
-
+        log.info(chatMessage.getMessage());
+        kafkaService.sendMessage(chatMessage);
         return chatMessage;
     }
 

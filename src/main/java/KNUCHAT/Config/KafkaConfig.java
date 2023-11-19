65dbe41533e95e2eec451e0f22a406925c181656
SangHee Lee <lsh277604@gmail.com>
2023-11-19 19:04:58 +0900

    Refactor: Value Serializer String -> json

--- src/main/java/KNUCHAT/Config/KafkaConfig.java
+++ src/main/java/KNUCHAT/Config/KafkaConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 @PropertySource("classpath:application-kafka.properties")
@@ -32,7 +33,7 @@ public class KafkaConfig {
 
         //Default
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
